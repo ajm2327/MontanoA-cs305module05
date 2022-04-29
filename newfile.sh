ff951c5a25629024b9ff094cb716654d9990a66f
ajm2327 <ajm2327@nau.edu>
2022-04-29 16:26:32 -0700

    new file

---
+++ newfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#I'm not sure if an sh file needs to start with that, but I type it anyway. Also I might be typing it wrong.
+
+echo "WAAAAAAAAAHHHH"
+
+echo "this repo aint got squat."
+
+
+
