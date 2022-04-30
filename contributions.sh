9477a020e4fbe6ef36763ad3efa6362f8d86b329
Shawn <sargentmtb@gmail.com>
2022-04-30 10:46:24 -0700

    I Contributed I think

--- contributions.sh
+++ contributions.sh
@@ -6,3 +6,4 @@ echo "hi its me Mario, this is my brother Luigi. You like nuts?"
 
 echo "angelic-penguin: howdy pardner"
 
+echo "ShawnSargent: This is a contribution to your project. Fear my power!"
