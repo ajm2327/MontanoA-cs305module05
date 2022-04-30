0443f3d4d587c8343df758e466eb5b60a34cb78a
Shawn Sargent <scs457@nau.edu>
2022-04-30 11:04:23 -0700

    pushing with right profile?

--- contributions.sh
+++ contributions.sh
@@ -6,4 +6,4 @@ echo "hi its me Mario, this is my brother Luigi. You like nuts?"
 
 echo "angelic-penguin: howdy pardner"
 
-echo "ShawnSargent: This is a contribution to your project. Fear my power!"
+echo "ShawnSargent: This is a contribution to your project. Fear my power a second time!"
