063b528bcc1129dcd53d0b146800aea3d53c5446
ShawnSargent <102007234+ShawnSargent@users.noreply.github.com>
2022-04-30 11:08:04 -0700

    Update contributions.sh

--- contributions.sh
+++ contributions.sh
@@ -7,3 +7,5 @@ echo "hi its me Mario, this is my brother Luigi. You like nuts?"
 echo "angelic-penguin: howdy pardner"
 
 echo "ShawnSargent: This is a contribution to your project. Fear my power a second time!"
+
+echo "pull request for Shawn"
